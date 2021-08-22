d6cebc627297c2ad5e7697bc9c93be073e36a8b9
mariagrosu <grosu.mari@mail.ru>
2021-08-22 12:48:23 +0300

    Add ViewControllers.

---
+++ CollectionViewPhotos/Controllers/ForYouViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ForYouViewController.swift
+//  CollectionViewPhotos
+//
+//  Created by Маша on 22.08.2021.
+//
+
+import Foundation
+import UIKit
+
+class ForYouViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
