08504d28750eb871973ff5d7b1890e22a50c94c4
mariagrosu <grosu.mari@mail.ru>
2021-08-22 12:52:59 +0300

    Change Model.

--- CollectionViewPhotos/Model/Model.swift
+++ CollectionViewPhotos/Model/Model.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+struct Section {
+    let type: String
+    let title: String
+    let items: [ModelItem]
+}
+
+struct ModelItem {
+    let title: String
+    let number: String
+    let nameImage: String
+}
